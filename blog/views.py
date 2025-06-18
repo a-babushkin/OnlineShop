5aa15d932d942f4aa32fabf610705cfc706dc1fc
a-babushkin <a.babushkin42@gmail.com>
2025-06-18 14:46:49 +0300

    Create Blog apps, model, migration, urls and views

---
+++ blog/views.py
@@ -0,0 +1,44 @@
+from django.db.models import F
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import BlogRecord
+
+
+class BlogRecordListView(ListView):
+    model = BlogRecord
+
+    def get_queryset(self):
+        return BlogRecord.objects.filter(is_published=True)
+
+
+class BlogRecordCreateView(CreateView):
+    model = BlogRecord
+    fields = ['title', 'content', 'image', 'is_published', 'views_number', 'published_date']
+    success_url = reverse_lazy('blog:blog_record_list')
+
+
+class BlogRecordDetailView(DetailView):
+    model = BlogRecord
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        # Чесно говорю, списал смутно догадывваюсь, что здесь происходит,
+        # но не знаю что такое атомарные операции вроде не проходили, но главное работает!
+        self.model.objects.filter(id=obj.id).update(views_number=F('views_number') + 1)
+        return obj
+
+
+class BlogRecordUpdateView(UpdateView):
+    model = BlogRecord
+    fields = ['title', 'content', 'image', 'is_published', 'views_number', 'published_date']
+    # template_name = 'blog_record_form.html'
+    # success_url = reverse_lazy('blog:blog_record_detail')
+
+    def get_success_url(self):
+        return reverse_lazy('blog:blog_record_detail', kwargs={'pk': self.object.pk})
+
+class BlogRecordDeleteView(DeleteView):
+    model = BlogRecord
+    # template_name = 'blog_record_delete.html'
+    success_url = reverse_lazy('blog:blog_record_list')
