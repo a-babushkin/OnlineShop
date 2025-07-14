f4bd8c2a1672bdcb74b206f7ddb919ae1dbfa3c2
a-babushkin <a.babushkin42@gmail.com>
2025-07-14 22:32:35 +0300

    Any corrections

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from email_validator import validate_email
 
 from .models import CustomUser
@@ -9,14 +9,14 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = ('email',)
+        fields = ("email",)
 
     # Стилизация полей формы
     def __init__(self, *args, **kwargs):
         super(CustomUserCreationForm, self).__init__(*args, **kwargs)
 
         self.fields["email"].widget.attrs.update(
-            {"class": "form-control", 'type': 'email', "placeholder": "Введите E-mail"}
+            {"class": "form-control", "type": "email", "placeholder": "Введите E-mail"}
         )
 
         self.fields["password1"].widget.attrs.update(
@@ -29,35 +29,49 @@ class CustomUserCreationForm(UserCreationForm):
 
     # Валидатор для проверки поля email
     def clean_email(self):
-        email = self.cleaned_data.get('email')
+        email = self.cleaned_data.get("email")
         if email and not validate_email(email):
-            raise forms.ValidationError('Неправильный формат Электронной почты.')
+            raise forms.ValidationError("Неправильный формат Электронной почты.")
         return email
 
 
 # Форма авторизации
 class CustomAuthenticationForm(AuthenticationForm):
-    username = forms.CharField(label="E-mail",
-                               widget=forms.EmailInput(attrs={'class': 'form-control', 'type': 'email'}))
+    username = forms.CharField(
+        label="E-mail",
+        widget=forms.EmailInput(attrs={"class": "form-control", "type": "email"}),
+    )
     password = forms.CharField(
         label="Password",
         strip=False,
         widget=forms.PasswordInput(
-            attrs={'autocomplete': 'current-password', 'class': 'form-control', 'type': 'password'}),
+            attrs={
+                "autocomplete": "current-password",
+                "class": "form-control",
+                "type": "password",
+            }
+        ),
     )
 
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = CustomUser
-        fields = ('email', 'first_name', 'last_name', 'phone_number', 'avatar', 'country')
+        fields = (
+            "email",
+            "first_name",
+            "last_name",
+            "phone_number",
+            "avatar",
+            "country",
+        )
 
     # Стилизация полей формы
     def __init__(self, *args, **kwargs):
         super(ProfileForm, self).__init__(*args, **kwargs)
 
         self.fields["email"].widget.attrs.update(
-            {"class": "form-control", 'type': 'email', "placeholder": "Ваш E-mail"}
+            {"class": "form-control", "type": "email", "placeholder": "Ваш E-mail"}
         )
 
         self.fields["first_name"].widget.attrs.update(
